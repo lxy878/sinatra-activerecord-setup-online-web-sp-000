9209ff8e1d0a7feddffb85de844cada89299a0d9
Xiaoyi Liu <lxy878@gmail.com>
2020-04-26 07:19:41 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 configure :development do
-  set :database, 'squ'
+  set :database, 'sqlite3:db/dogs.db'
 end
 
 require './app'
